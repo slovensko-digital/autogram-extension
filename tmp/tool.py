cff576bd0649c39815d92707a7e6d1ccfd8ff301
pom <>
2021-09-21 11:45:06 +0200

    Cleanup

--- tmp/tool.py
+++ tmp/tool.py
@@ -4,36 +4,33 @@ import zipfile
 
 from pyasice import Container, XmlSignature
 
-
-filename = 'output6'
-
-def base64txt2asice():
-  with open(filename + '.txt', 'rb') as infile:
-    with open(filename + '.asice', 'wb') as outfile:
-      print(base64.decode(infile, outfile))
+def base64txt2asice(filename):
+    with open(filename + '.txt', 'rb') as infile:
+        with open(filename + '.asice', 'wb') as outfile:
+            print(base64.decode(infile, outfile))
 
 
-def listAsice():
-  #container = Container.open('/Users/pom/Downloads/aaa-test-aaa.asice')
-  container = Container.open(filename + '.asice')
-  print(container.verify_signatures())
-  print(list(container.iter_data_files()))
-  for xmlsig in container.iter_signatures():
-      print(xmlsig)
-      print(xmlsig.dump())
-      print(xmlsig.get_signing_time())
+def listAsice(filename):
+    container = Container.open(filename + '.asice')
+    print(container.verify_signatures())
+    print(list(container.iter_data_files()))
+    for xmlsig in container.iter_signatures():
+        print(xmlsig)
+        print(xmlsig.dump())
+        print(xmlsig.get_signing_time())
 
 
-def unzipAsice():
-    with zipfile.ZipFile(filename+'.asice', 'r') as zipref:
+def unzipAsice(filename):
+    with zipfile.ZipFile(filename + '.asice', 'r') as zipref:
         zipref.extractall(filename + '.unzip')
 
-def main():
-    base64txt2asice()
-    unzipAsice()
-    listAsice()
 
+def main():
+    filename = 'output6'
+    base64txt2asice(filename)
+    unzipAsice(filename)
+    listAsice(filename)
 
 
 if __name__ == "__main__":
-  main()
+    main()
