43fd8bb15645af26c44fb3a55d207defe738eff3
pom <stanislavparnicky@gmail.com>
2022-01-05 02:21:14 +0100

    Cleanup for firefox

--- webpack/manifest.ts
+++ webpack/manifest.ts
@@ -1,5 +1,7 @@
 import { Options as WMPOptions, FileDescriptor } from "webpack-manifest-plugin";
 
+const manifestVersion: 2 | 3 = 2;
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require("../package.json");
 
@@ -11,63 +13,110 @@ function generateManifest(
   // console.log(seed);
   // console.log(files);
   // console.log(entries);
-  return {
-    manifest_version: 3, //2
+
+  const common = {
     name: packageJson.name,
     version: packageJson.version,
     description: packageJson.description,
     author: "pom",
+  };
 
-    permissions: [
-      "storage",
-      "webRequest",
-      // "webRequestBlocking", //2
-      "webNavigation",
-      "scripting", //3
-      "tabs", // 3
-      "activeTab", //3
-    ],
+  switch (manifestVersion) {
+    case 3:
+      return {
+        manifest_version: 3, //2
 
-    // icons: {
-    //   "128": "logo.png",
-    // },
-    // options_page: "src/ui/options.html",
-    content_scripts: [
-      {
-        matches: [
-          "https://www.slovensko.sk/*",
-          "https://schranka.slovensko.sk/*",
-          "https://pfseform.financnasprava.sk/*",
+        ...common,
+
+        permissions: [
+          "storage",
+          "webRequest",
+          // "webRequestBlocking", //2
+          "webNavigation",
+          "scripting", //3
+          "tabs", // 3
+          "activeTab", //3
         ],
-        js: entries.content,
-      },
-    ],
-    // background: {
-    //   scripts: entries.background,
-    //   persistent: true,
-    // },
-    // background: {
-    //   service_worker: entries.background,
-    // },
-    // action: {
-    //   default_icon: "static/logo.png",
-    //   default_popup: "popup.html",
-    // },
-    web_accessible_resources: [
-      {
-        resources: [
+
+        // icons: {
+        //   "128": "logo.png",
+        // },
+        // options_page: "src/ui/options.html",
+        content_scripts: [
+          {
+            matches: [
+              "https://www.slovensko.sk/*",
+              "https://schranka.slovensko.sk/*",
+              "https://pfseform.financnasprava.sk/*",
+              "https://pfseform.financnasprava.sk",
+            ],
+            js: entries.content,
+          },
+        ],
+        // background: {
+        //   scripts: entries.background,
+        //   persistent: true,
+        // },
+        // background: {
+        //   service_worker: entries.background,
+        // },
+        // action: {
+        //   default_icon: "static/logo.png",
+        //   default_popup: "popup.html",
+        // },
+        web_accessible_resources: [
+          {
+            resources: [
+              ...entries.inject.map((x) => [x, x + ".map"]).flat(),
+              ...entries.content.map((x) => [x, x + ".map"]).flat(),
+              "static/logo.png",
+            ],
+            matches: [
+              "https://www.slovensko.sk/*",
+              "https://schranka.slovensko.sk/*",
+              "https://pfseform.financnasprava.sk/*",
+              "https://pfseform.financnasprava.sk",
+            ],
+          },
+        ],
+      };
+
+    case 2:
+    default:
+      return {
+        manifest_version: 2,
+
+        ...common,
+
+        permissions: [
+          "storage",
+          "webRequest",
+          "webNavigation",
+          // "tabs", // 3
+          // "activeTab", //3
+        ],
+        content_scripts: [
+          {
+            matches: [
+              "https://www.slovensko.sk/*",
+              "https://schranka.slovensko.sk/*",
+              "https://pfseform.financnasprava.sk/*",
+              "https://pfseform.financnasprava.sk",
+            ],
+            js: entries.content,
+          },
+        ],
+        web_accessible_resources: [
           ...entries.inject.map((x) => [x, x + ".map"]).flat(),
           ...entries.content.map((x) => [x, x + ".map"]).flat(),
           "static/logo.png",
-        ],
-        matches: [
           "https://www.slovensko.sk/*",
           "https://schranka.slovensko.sk/*",
           "https://pfseform.financnasprava.sk/*",
+          "https://pfseform.financnasprava.sk",
         ],
-      },
-    ],
-  };
+      };
+  }
 }
 
 export const manifestOptions: WMPOptions = {
