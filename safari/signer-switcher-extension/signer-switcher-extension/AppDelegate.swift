ab6183cdd747da9d97c97ab463454b41adbd12a6
pom <stanislavparnicky@gmail.com>
2022-01-26 12:12:27 +0100

    Add Safari Web Extension Xcode project

---
+++ safari/signer-switcher-extension/signer-switcher-extension/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  signer-switcher-extension
+//
+//  Created by pom on 26/01/2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
