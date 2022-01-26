07786f44baa672b65911ffe8a8facb0a2f9d4368
pom <stanislavparnicky@gmail.com>
2022-01-26 12:12:55 +0100

    Handle Safari https

--- src/util.ts
+++ src/util.ts
@@ -2,3 +2,8 @@ export function TODO(...rest: any[]): void {
   // eslint-disable-next-line prefer-rest-params
   console.debug("TODO:", rest, arguments);
 }
+
+
+export function isSafari(): boolean{
+  return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+}
