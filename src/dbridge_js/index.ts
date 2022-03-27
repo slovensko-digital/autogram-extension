756318bf2a4410e54e88c525dcdb74ff64e0adb4
pom <stanislavparnicky@gmail.com>
2022-03-28 01:23:12 +0200

    Test proxy

--- src/dbridge_js/index.ts
+++ src/dbridge_js/index.ts
@@ -1,14 +1,24 @@
 import { ditecX } from "./ditecx";
-import { wrapWithProxy } from "./proxy";
 
-type OriginalDitec = unknown;
+type OriginalDitec = object;
 
+const useProxy = true;
+const useProxyWithOriginal = useProxy && false;
 export function inject(windowAny: { ditec?: OriginalDitec }): void {
   console.log("Start inject");
   console.log(windowAny.ditec);
 
   if (windowAny.ditec) {
-    windowAny.ditec = ditecX;
+    if (useProxy) {
+      import("./proxy").then(({ wrapWithProxy }) => {
+        windowAny.ditec = useProxyWithOriginal
+          ? wrapWithProxy(windowAny.ditec)
+          : wrapWithProxy(ditecX);
+      });
+    } else {
+      windowAny.ditec = ditecX;
+    }
+
     // windowAny.ditec = wrapWithProxy(ditecX);
   }
 }
--- src/dbridge_js/proxy.ts
+++ src/dbridge_js/proxy.ts
@@ -1,56 +1,104 @@
 const log = [];
 
+const ignoredProps: PropertyKey[] = [
+  "__implementation",
+  Symbol.toStringTag,
+  Symbol.iterator,
+  "$$typeof",
+  "@@__IMMUTABLE_RECORD__@@",
+  "hasAttribute",
+  "tagName",
+  "nodeType",
+  "onSuccess",
+  "toString",
+  Symbol.toPrimitive,
+  Symbol.toStringTag,
+  "splice",
+  "prototype",
+];
+
+const logger = {
+  push(...rest) {
+    // console.log(...rest);
+    log.push(...rest);
+  },
+};
+
 function getSpyHandler(prefix?: string, depth?: number) {
   const spyHandler = {
-    get(target, prop, receiver) {
-      console.log("get", typeof target, target, prop, receiver, prefix);
+    get(target: object, prop: PropertyKey, receiver?: unknown) {
       //   log.push({ target, prefix, prop, receiver });
       //   const wrappedTarget =
       //     typeof target === "function" ? wrapFunction(target) : target;
 
-      if (prop == "__proxy_name") {
+      if (prop === "__proxy_name") {
         return prefix;
+      }
+
+      if (prop === "__proxy_log") {
+        return log;
       }
 
       const name = `${prefix}.${prop.toString()}`;
       const reflection = Reflect.get(target, prop, receiver);
-      return depth < 3 && prop !== "__implementation"
+
+      if (typeof reflection == "function")
+        logger.push({
+          type: "get",
+          name,
+          target,
+          prop,
+          receiver,
+        });
+
+      return depth < 3 &&
+        !ignoredProps.includes(prop) &&
+        (typeof reflection == "function" || typeof reflection == "object")
         ? wrapWithProxy(reflection, name, depth + 1)
         : reflection;
     },
-    construct(target, argumentList, newTarget) {
-      console.log("construct", target, argumentList, newTarget);
-      return Reflect.construct(target, argumentList, newTarget);
-    },
-    apply(target, thisArg, argumentList) {
-      console.log("apply", typeof target, target, thisArg, argumentList);
-      const [fnName, argTail] = argumentList;
-      const name = `${thisArg.__proxy_name}.${fnName}`;
 
-    // this causes infinite loop
-    //   if (
-    //     argTail &&
-    //     argTail[0] &&
-    //     argTail[0]["onSuccess"] &&
-    //     typeof argTail[0]["onSuccess"] === "function"
-    //   ) {
-    //     argTail[0]["onSuccess"] = function (...onSuccessArgs) {
-    //       console.log(`callback ${name}:onSuccess`, onSuccessArgs);
-    //       return argTail[0]["onSuccess"](...onSuccessArgs);
-    //     };
-    //   }
-      console.log(`calling ${name}`, argTail);
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    // construct(target: Function, argumentList, newTarget: Function) {
+    //   console.log("construct", target, argumentList, newTarget);
+    //   return Reflect.construct(target, argumentList, newTarget);
+    // },
+
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    apply(target: Function, thisArg, argumentList) {
+      const name = `${thisArg.__proxy_name}.${target.name}`;
+
+      // this causes infinite loop
+      if (
+        argumentList &&
+        argumentList[0] &&
+        argumentList[0]["onSuccess"] &&
+        typeof argumentList[0]["onSuccess"] === "function"
+      ) {
+        const oldOnSuccess = argumentList[0]["onSuccess"];
+        argumentList[0]["onSuccess"] = function (...onSuccessArgs) {
+          logger.push({ type: "callback.onSuccess", name, onSuccessArgs });
+          return oldOnSuccess(...onSuccessArgs);
+        };
+      }
+
+      logger.push({ type: "apply", name, target, thisArg, argumentList });
       return Reflect.apply(target, thisArg, argumentList);
     },
   };
   return spyHandler;
 }
 
-export function wrapWithProxy(target: any, prefix = "", depth = 0) {
-  console.log(target);
+export function wrapWithProxy<T extends object>(
+  target: T,
+  prefix = "root",
+  depth = 0
+): T & { __proxy_log: typeof log } {
   if (typeof target !== "object" && typeof target !== "function") {
-    console.log("skipping", typeof target, target);
+    // console.log("skipping", typeof target, target);
     return target;
   }
-  return new Proxy(target, getSpyHandler(prefix, depth));
+  return new Proxy(target, getSpyHandler(prefix, depth)) as T & {
+    __proxy_log: typeof log;
+  };
 }
