511d596f0af7ecf27c8fedd92db9d0b22732c08f
pom <stanislavparnicky@gmail.com>
2023-02-25 21:19:44 +0100

    build: remove npm publish from release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,7 +9,6 @@ if [ ! -z "$VERSION" ]; then
 
     ## Publish Package
     npm version $VERSION -m "Bump version to: %s [skip ci]"
-    npm publish
 
     ## Create GitHub Release
     git push --follow-tags --set-upstream origin $branch
