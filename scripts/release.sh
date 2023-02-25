671354b6728133904cf82106d52600c2009d7eb0
pom <stanislavparnicky@gmail.com>
2023-02-25 20:00:41 +0100

    fix: move release into script

---
+++ scripts/release.sh
@@ -0,0 +1,16 @@
+VERSION=$(npx auto version)
+
+## Support for label 'skip-release'
+if [ ! -z "$VERSION" ]; then
+    npm run build:prod
+    ## Update Changelog
+    npx auto changelog
+
+    ## Publish Package
+    npm version $VERSION -m "Bump version to: %s [skip ci]"
+    npm publish
+
+    ## Create GitHub Release
+    git push --follow-tags --set-upstream origin $branch
+    npx auto release
+fi
