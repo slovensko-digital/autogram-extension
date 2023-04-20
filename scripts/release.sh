c792fea0982d1a0ea9ff361c20cd65ded53dcff8
pom <stanislavparnicky@gmail.com>
2023-04-20 16:52:36 +0200

    build: fix relase script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,13 +2,17 @@ VERSION=$(npx auto version)
 
 ## Support for label 'skip-release'
 if [ ! -z "$VERSION" ]; then
-    npm run clean
+    # test build
     npm run build:prod
+
     ## Update Changelog
     npx auto changelog
 
     ## Publish Package
     npm version $VERSION -m "Bump version to: %s [skip ci]"
+
+    npm run clean
+    npm run build:prod
 
     ## Create GitHub Release
     git push --follow-tags --set-upstream origin $branch
