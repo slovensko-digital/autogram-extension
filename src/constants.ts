7fc4a10f05e71f7d4eab21ae1e5604610b322085
Marek Bodinger <marek.bodinger@gmail.com>
2023-12-05 11:31:23 +0100

    Add bratislava.sk websites to supported list

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,8 @@ export const enabledUrls = [
   "https://schranka.slovensko.sk/*",
   "https://pfseform.financnasprava.sk/*",
   "https://www.financnasprava.sk/*",
+  "https://city-account-next.dev.bratislava.sk/*",
+  "https://city-account-next.staging.bratislava.sk/*",
+  "https://konto.bratislava.sk/*",
   ...(process.env.NODE_ENV !== "production" ? ["http://localhost:3000/*"] : []),
 ];
